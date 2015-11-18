d6e87bebe6d02240780ce9fccda57732baa29d31
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-11-18 13:27:36 +0100

    Added XML doc and Dismiss made public

--- src/RadialMenu/RadialMenu/ALRadialMenu.cs
+++ src/RadialMenu/RadialMenu/ALRadialMenu.cs
@@ -19,8 +19,7 @@ namespace DK.Ostebaronen.Touch.RadialMenu
         private Angle _circumference = new Angle { Degrees = 360f };
         private Angle _spacingDegrees;
         private CGPoint _animationOrigin;
-        private UIViewAnimationOptions _animationOptions =
-            UIViewAnimationOptions.CurveEaseInOut | UIViewAnimationOptions.BeginFromCurrentState;
+        private const UIViewAnimationOptions AnimationOptions = UIViewAnimationOptions.CurveEaseInOut | UIViewAnimationOptions.BeginFromCurrentState;
 
         private IList<ALRadialMenuButton> Buttons
         {
@@ -42,13 +41,8 @@ namespace DK.Ostebaronen.Touch.RadialMenu
             }
         }
 
-        public ALRadialMenu()
-            : base(CGRect.Empty)
-        { CommonInit(); }
-
-        public ALRadialMenu(CGRect frame)
-            : base(frame)
-        { CommonInit(); }
+        public ALRadialMenu() : base(CGRect.Empty) { CommonInit(); }
+        public ALRadialMenu(CGRect frame) : base(frame) { CommonInit(); }
 
         private void CommonInit()
         {
@@ -60,12 +54,22 @@ namespace DK.Ostebaronen.Touch.RadialMenu
             _overlayView.AddGestureRecognizer(_dismissGesture);
         }
 
+        /// <summary>
+        /// Set the delay between each of the buttons appearing.
+        /// </summary>
+        /// <param name="delay"><see cref="double"/> with the delay in ms</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetDelay(double delay)
         {
             _delay = delay;
             return this;
         }
 
+        /// <summary>
+        /// Set the buttons to present.
+        /// </summary>
+        /// <param name="buttons"><see cref="IList{T}"/> of <see cref="ALRadialMenuButton"/>.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetButtons(IList<ALRadialMenuButton> buttons)
         {
             Buttons = buttons;
@@ -86,6 +90,13 @@ namespace DK.Ostebaronen.Touch.RadialMenu
             return this;
         }
 
+        /// <summary>
+        /// Set whether to dismiss the menu when tapping elsewhere in the view.
+        /// 
+        /// Default: <value>true</value>
+        /// </summary>
+        /// <param name="dismissOnOverlayTap"><see cref="bool"/></param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetDismissOnOverlayTap(bool dismissOnOverlayTap)
         {
             _dismissOnOverlayTap = dismissOnOverlayTap;
@@ -94,32 +105,68 @@ namespace DK.Ostebaronen.Touch.RadialMenu
             return this;
         }
 
+        /// <summary>
+        /// Set the radius to present the menu in.
+        /// 
+        /// Default: <value>100</value>
+        /// </summary>
+        /// <param name="radius"><see cref="float"/> with the radius.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetRadius(float radius)
         {
             _radius = radius;
             return this;
         }
 
+        /// <summary>
+        /// Set the start angle of the angle to start present the menu.
+        /// 
+        /// Default: <value>270</value>
+        /// </summary>
+        /// <param name="degrees"><see cref="float"/> with the start angle.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetStartAngle(float degrees)
         {
             _startAngle = new Angle { Degrees = degrees };
             return this;
         }
 
+        /// <summary>
+        /// Set the circumference of the menu.
+        /// 
+        /// Default: <value>360</value>
+        /// </summary>
+        /// <param name="degrees"><see cref="float"/> with the circumference.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetCircumference(float degrees)
         {
             Circumference = new Angle { Degrees = degrees };
             return this;
         }
 
+        /// <summary>
+        /// Set the animation origin. The point to center the menu around.
+        /// </summary>
+        /// <param name="animationOrigin"><see cref="CGPoint"/> with the point on the screen to center the menu around.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu SetAnimationOrigin(CGPoint animationOrigin)
         {
             _animationOrigin = animationOrigin;
             return this;
         }
 
+        /// <summary>
+        /// Present the menu in the <see cref="UIView"/>.
+        /// </summary>
+        /// <param name="view"><see cref="UIView"/> to present the menu in.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu PresentInView(UIView view) { return PresentInWindow(view.Window); }
 
+        /// <summary>
+        /// Present the menu in the <see cref="UIWindow"/>.
+        /// </summary>
+        /// <param name="window"><see cref="UIWindow"/> to present the menu in.</param>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
         public ALRadialMenu PresentInWindow(UIWindow window)
         {
             if (Buttons == null)
@@ -144,7 +191,11 @@ namespace DK.Ostebaronen.Touch.RadialMenu
             return this;
         }
 
-        private ALRadialMenu Dismiss()
+        /// <summary>
+        /// Dismiss the menu.
+        /// </summary>
+        /// <returns><see cref="ALRadialMenu"/> for method chaining.</returns>
+        public ALRadialMenu Dismiss()
         {
             if (Buttons == null || Buttons.Count == 0)
             {
@@ -178,7 +229,7 @@ namespace DK.Ostebaronen.Touch.RadialMenu
 
             view.Center = _animationOrigin;
             view.Alpha = 0;
-            AnimateNotify(0.5, delay, 0.7f, 0.7f, _animationOptions, () => {
+            AnimateNotify(0.5, delay, 0.7f, 0.7f, AnimationOptions, () => {
                 view.Alpha = 1;
                 view.Center = newCenter;
             }, null);
@@ -188,7 +239,7 @@ namespace DK.Ostebaronen.Touch.RadialMenu
         {
             var delay = index * _delay;
 
-            AnimateNotify(0.5, delay, 0.7f, 0.7f, _animationOptions,
+            AnimateNotify(0.5, delay, 0.7f, 0.7f, AnimationOptions,
                 () => {
                     view.Alpha = 0;
                     view.Center = _animationOrigin;
@@ -200,7 +251,7 @@ namespace DK.Ostebaronen.Touch.RadialMenu
 
         private void SelectedAnimation(UIView view)
         {
-            AnimateNotify(0.5, 0, 0.7f, 0.7f, _animationOptions,
+            AnimateNotify(0.5, 0, 0.7f, 0.7f, AnimationOptions,
                 () => {
                     view.Alpha = 0;
                     view.Transform = CGAffineTransform.MakeScale(1.5f, 1.5f);
